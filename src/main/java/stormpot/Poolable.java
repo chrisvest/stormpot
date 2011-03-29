9abb5c8f611c8fe79d4e04a86275eee4f9b908ba
Chris Vest <karmazilla@gmail.com>
2011-03-29 10:09:55 +0200

    javadoc for the Poolable interface and contract.

--- src/main/java/stormpot/Poolable.java
+++ src/main/java/stormpot/Poolable.java
@@ -1,7 +1,49 @@
 package stormpot;
 
+/**
+ * Objects contained in a {@link Pool} must implement the Poolable interface
+ * and adhere to its contract.
+ * <p>
+ * Pools call {@link Allocator#allocate(Slot) allocate} on Allocators with the
+ * specific {@link Slot} that they want a Poolable allocated for. The Slot
+ * represents a location in the pool, that can fit an object and make it
+ * available for others to claim.
+ * <p>
+ * The contract of the Poolable interface is, that when {@link #release()
+ * release} is called on the Poolable, it must in turn call
+ * {@link Slot#release() release} on the specific Slot object that it was
+ * allocated with.
+ * <p>
+ * The simplest possible correct implementation of the Poolable interface looks
+ * like this:
+ * <pre><code> public class GenericPoolable implements Poolable {
+ *   private final Slot slot;
+ *   public GenericPoolable(Slot slot) {
+ *     this.slot = slot;
+ *   }
+ *   
+ *   public void release() {
+ *     slot.release();
+ *   }
+ * }</code></pre>
+ * Pools in turn guarantee these memory effects:
+ * <ul>
+ * <li>The {@link Poolable#release() release} of an object happens-before
+ * any subsequent claim of that object, and,
+ * <li>The {@link Allocator#allocate(Slot) allocation} of an object
+ * happens-before any claim of that object.
+ * </ul>
+ * @author Chris Vest &lt;mr.chrisvest@gmail.com&gt;
+ *
+ */
 public interface Poolable {
-
+  /**
+   * Release this Poolable object back into the pool from where it came,
+   * so that others can claim it, or the pool deallocate it if it has
+   * expired.
+   * <p>
+   * A call to this method MUST delegate to a call to {@link Slot#release()
+   * release} on the Slot object for which this Poolable was allocated.
+   */
   void release();
-
 }
