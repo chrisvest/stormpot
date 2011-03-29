ed6457995c5e4665727f521364dab20e82deca83
Chris Vest <karmazilla@gmail.com>
2011-03-29 14:22:36 +0200

    remove dead code from PoolException - besides, throwing an exception without attaching any message to it is just bad style.

--- src/main/java/stormpot/PoolException.java
+++ src/main/java/stormpot/PoolException.java
@@ -16,15 +16,8 @@ package stormpot;
 public class PoolException extends RuntimeException {
   private static final long serialVersionUID = -1908093409167496640L;
 
-  public PoolException() {
-  }
-
   public PoolException(String message) {
     super(message);
-  }
-
-  public PoolException(Throwable cause) {
-    super(cause);
   }
 
   public PoolException(String message, Throwable cause) {
