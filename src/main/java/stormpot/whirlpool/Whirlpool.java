7f43ab054265c30e2fbaeae42955750f261cf128
Chris Vest <karmazilla@gmail.com>
2011-04-08 22:24:27 +0200

    I think the flat-combining technique might work pretty well for the Whirlpool implementation, that is intended to be "faster than new Object (if at all possible)"

---
+++ src/main/java/stormpot/whirlpool/Whirlpool.java
@@ -0,0 +1,14 @@
+package stormpot.whirlpool;
+
+import stormpot.Pool;
+
+/**
+ * Whirlpool is a {@link Pool} that is based on a queue-like structure, made
+ * concurrent using the Flat-Combining technique of Hendler, Incze, Shavit and
+ * Tzafrir.
+ * @author Chris Vest &lt;mr.chrisvest@gmail.com&gt;
+ *
+ */
+public class Whirlpool {
+  
+}
