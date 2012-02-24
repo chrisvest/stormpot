07bc4cd0ff80c3d4994f548165aab67d8b05e365
Chris Vest <karmazilla@gmail.com>
2012-02-24 17:07:45 +0100

    SlotInfo javadoc.

--- src/main/java/stormpot/SlotInfo.java
+++ src/main/java/stormpot/SlotInfo.java
@@ -8,10 +8,23 @@ package stormpot;
  */
 public interface SlotInfo<T extends Poolable> {
 
+  /**
+   * Get the approximate number of milliseconds that have transpired since the
+   * object was allocated.
+   * @return The age of the object in milliseconds.
+   */
   long getAgeMillis();
 
+  /**
+   * Get the number of times the object has been claimed since it was
+   * allocated.
+   * @return The objects claim count.
+   */
   long getClaimCount();
 
+  /**
+   * Get the Poolable object represented by this SlotInfo instance.
+   * @return The Poolable being examined for validity. Never null.
+   */
   T getPoolable();
-
 }
