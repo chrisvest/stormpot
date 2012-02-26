1916a5c67d07728f3d727fc0f3bf5dece1b1955b
Chris Vest <karmazilla@gmail.com>
2012-02-26 21:02:16 +0100

    toss the Caliper stuffs.

--- benchmark.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo -n '(building classpath... '
-export CP=$(mvn --offline dependency:build-classpath|grep .m2)
-echo 'done)'
-java -cp target/classes:target/test-classes:$CP \
-  com.google.caliper.Runner $*
-
