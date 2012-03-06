134759786cbd33e18de980eb7e9f4ca7626c3de3
Chris Vest <karmazilla@gmail.com>
2012-03-06 20:28:35 +0100

    docupdate hack to make jacoco report show images on github.

--- docupdate.sh
+++ docupdate.sh
@@ -3,6 +3,11 @@ mvn clean install site
 git co gh-pages
 rm -fr site
 cp -fr target/site .
+
+# fix jacoco resources
+mv site/jacoco/.resources site/jacoco/resources
+perl -e 's/\.resources/resources/g;' -pi $(find site/jacoco -name '*.html')
+
 git add -A site
 git ci -m 'maven site update.'
 git push
