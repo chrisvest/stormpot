2310bcdd997f04d562446c86ff2dfcfe0dd6c1a2
Chris Vest <karmazilla@gmail.com>
2012-06-26 18:51:33 +0200

    simplify the multi-threaded benchmark by not reusing threads, hoping to be rid of that occasional liveness failure.

--- stormpot-benchmark/src/main/java/stormpot/benchmark/MultiThreadedThroughputBenchmark.java
+++ stormpot-benchmark/src/main/java/stormpot/benchmark/MultiThreadedThroughputBenchmark.java
@@ -2,13 +2,13 @@ package stormpot.benchmark;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.LockSupport;
 
 public class MultiThreadedThroughputBenchmark extends Benchmark {
   private static final int THREAD_COUNT = Integer.getInteger("thread.count",
         Runtime.getRuntime().availableProcessors());
 
   private WorkerThread[] threads;
+  private volatile CountDownLatch startLatch;
   private volatile CountDownLatch doneLatch;
   
   @Override
@@ -35,21 +35,13 @@ public class MultiThreadedThroughputBenchmark extends Benchmark {
   @Override
   protected void beforeBenchmark(Bench bench, long trialTimeMillis) {
     super.beforeBenchmark(bench, trialTimeMillis);
-    ensureThreadsCreated();
+    
+    startLatch = new CountDownLatch(1);
     doneLatch = new CountDownLatch(THREAD_COUNT);
-    for (WorkerThread th : threads) {
-      th.bench = bench;
-      th.doneLatch = doneLatch;
-    }
-  }
-
-  protected void ensureThreadsCreated() {
-    if (threads == null) {
-      threads = new WorkerThread[THREAD_COUNT];
-      for (int i = 0; i < THREAD_COUNT; i++) {
-        threads[i] = new WorkerThread();
-        threads[i].start();
-      }
+    threads = new WorkerThread[THREAD_COUNT];
+    for (int i = 0; i < THREAD_COUNT; i++) {
+      threads[i] = new WorkerThread(bench, startLatch, doneLatch);
+      threads[i].start();
     }
   }
 
@@ -57,60 +49,47 @@ public class MultiThreadedThroughputBenchmark extends Benchmark {
   protected void benchmark(Bench bench, long trialTimeMillis) throws Exception {
     long start = System.currentTimeMillis();
     long deadline = start + trialTimeMillis;
-    start(threads, deadline);
+    for (WorkerThread th : threads) {
+      th.deadline = deadline;
+    }
+    startLatch.countDown();
     doneLatch.await();
     long end = System.currentTimeMillis();
     bench.recordPeriod(end - start);
   }
   
-  private void start(WorkerThread[] threads, long deadline) {
-    for (WorkerThread th : threads) {
-      th.deadline = deadline;
-      LockSupport.unpark(th);
-    }
-  }
-
   private static class WorkerThread extends Thread {
     private static final AtomicInteger counter = new AtomicInteger();
-    public Bench bench;
     public volatile long deadline;
-    public volatile CountDownLatch doneLatch;
+    private final Bench bench;
+    private final CountDownLatch doneLatch;
+    private final CountDownLatch startLatch;
     
-    public WorkerThread() {
+    public WorkerThread(Bench bench, CountDownLatch startLatch, CountDownLatch doneLatch) {
       super("WorkerThread-" + counter.incrementAndGet());
       setDaemon(true);
+      this.bench = bench;
+      this.startLatch = startLatch;
+      this.doneLatch = doneLatch;
     }
     
     @Override
     public void run() {
       try {
-        runInterruptibly();
-      } catch (InterruptedException e) {
-        // and now we're done...
+        startLatch.await();
+        doBenchmarkWork();
+        doneLatch.countDown();
       } catch (Exception e) {
         e.printStackTrace();
       }
     }
 
-    private void runInterruptibly() throws Exception {
-      for (;;) {
-        waitForStartingSignal();
-        doBenchmarkWork();
-      }
-    }
-
-    private void waitForStartingSignal() {
-      LockSupport.park();
-    }
-
     protected void doBenchmarkWork() throws Exception {
-      CountDownLatch doneLatch = this.doneLatch;
       Bench bench = this.bench;
       long deadline = this.deadline;
       do {
         runCycles(bench, 100);
       } while (System.currentTimeMillis() < deadline);
-      doneLatch.countDown();
     }
   }
 }
