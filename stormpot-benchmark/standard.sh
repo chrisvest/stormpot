23054b826987efa95fd710c0356db1420735002f
Chris Vest <karmazilla@gmail.com>
2012-06-29 23:10:00 +0200

    make the standard.sh benchmark generate far fewer files - much easier to import into a spreadsheet this way.

--- stormpot-benchmark/standard.sh
+++ stormpot-benchmark/standard.sh
@@ -1,28 +1,31 @@
 
 mkdir -p data
-export MSG='-Dreport.msg=## %s,%7d,%3d,%7.0f,%3d,%.6f,%s,%.6f%n'
 
 function mark-single {
+  export MSG='-Dreport.msg=## %s,%7d,%3d,%7.0f,%3d,%.6f,%s,%.6f%n'
+  echo '' > "data/$1-single.csv"
   echo "Single-threaded $1 benchmark..."
   echo '1/3... '
-  mvn -Dthroughput-single "-Dpools=$1" "$MSG" | grep '##' | tee "data/$1-single-1.csv"
+  mvn -Dthroughput-single "-Dpools=$1" "$MSG" | grep '##' | tee -a "data/$1-single.csv"
   echo '2/3... '
-  mvn -Dthroughput-single "-Dpools=$1" "$MSG" | grep '##' | tee "data/$1-single-2.csv"
+  mvn -Dthroughput-single "-Dpools=$1" "$MSG" | grep '##' | tee -a "data/$1-single.csv"
   echo '3/3... '
-  mvn -Dthroughput-single "-Dpools=$1" "$MSG" | grep '##' | tee "data/$1-single-3.csv"
+  mvn -Dthroughput-single "-Dpools=$1" "$MSG" | grep '##' | tee -a "data/$1-single.csv"
   echo 'done.'
 }
 
 function mark-multi {
+  export MSG='-Dreport.msg=## %s,$n,%7d,%3d,%7.0f,%3d,%.6f,%s,%.6f%n'
+  echo '' > "data/$1.csv"
   for n in 1 2 3 4 5 6 8 10 12 14 16
   do
     echo "Benchmarking $1 with $n threads..."
     echo '1/3... '
-    mvn -Dthroughput-multi "-Dthread.count=$n" "-Dpools=$1" "$MSG" | grep '##' | tee "data/$1-t$n-1.csv"
+    mvn -Dthroughput-multi "-Dthread.count=$n" "-Dpools=$1" "$MSG" | grep '##' | tee -a "data/$1.csv"
     echo '2/3... '
-    mvn -Dthroughput-multi "-Dthread.count=$n" "-Dpools=$1" "$MSG" | grep '##' | tee "data/$1-t$n-2.csv"
+    mvn -Dthroughput-multi "-Dthread.count=$n" "-Dpools=$1" "$MSG" | grep '##' | tee -a "data/$1.csv"
     echo '3/3... '
-    mvn -Dthroughput-multi "-Dthread.count=$n" "-Dpools=$1" "$MSG" | grep '##' | tee "data/$1-t$n-3.csv"
+    mvn -Dthroughput-multi "-Dthread.count=$n" "-Dpools=$1" "$MSG" | grep '##' | tee -a "data/$1.csv"
     echo 'done.'
   done
 }
