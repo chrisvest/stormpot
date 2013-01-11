d09d64c81462974435ae6a3b5f516e4ebfa951f1
Chris Vest <karmazilla@gmail.com>
2013-01-11 19:51:24 +0100

    elaborated on the purpose of the FailurePrinterTestRule.

--- src/test/java/stormpot/FailurePrinterTestRule.java
+++ src/test/java/stormpot/FailurePrinterTestRule.java
@@ -8,6 +8,11 @@ import org.junit.runners.model.Statement;
  * A TestRule that ensures that any failing tests have their stack-trace
  * printed to stderr.
  * 
+ * This is useful for when the tests are running on a build-server, and you'd
+ * like the details of any failure to be printed to the build-log. A nice thing
+ * if the build system does not make the build artifacts with the test failures
+ * available.
+ * 
  * @author cvh
  */
 public class FailurePrinterTestRule implements TestRule {
