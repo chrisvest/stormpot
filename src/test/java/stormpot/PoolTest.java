cd58fae50557f32a49fa6090e20097c648269f21
Chris Vest <mr.chrisvest@gmail.com>
2014-01-06 20:30:50 +0100

    Put the ResizablePoolTests into the PoolTest class.

--- src/test/java/stormpot/PoolTest.java
+++ src/test/java/stormpot/PoolTest.java
@@ -26,6 +26,7 @@ import org.junit.runner.RunWith;
 import stormpot.bpool.BlazePoolFixture;
 import stormpot.qpool.QueuePoolFixture;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -38,6 +39,7 @@ import java.util.concurrent.locks.ReentrantLock;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
+import static org.junit.Assume.assumeThat;
 import static stormpot.UnitKit.*;
 
 /**
@@ -95,6 +97,12 @@ public class PoolTest {
   setUp() {
     allocator = new CountingAllocator();
     config = new Config<GenericPoolable>().setSize(1).setAllocator(allocator);
+  }
+
+  private ResizablePool<GenericPoolable> resizable(PoolFixture fixture) {
+    Pool<GenericPoolable> pool = fixture.initPool(config);
+    assumeThat(pool, instanceOf(ResizablePool.class));
+    return (ResizablePool<GenericPoolable>) pool;
   }
   
   @Test(expected = IllegalArgumentException.class)
@@ -984,8 +992,7 @@ public class PoolTest {
   throws Exception {
     Pool<GenericPoolable> pool = fixture.initPool(config);
     assertNotNull("Did not deplete pool in time", pool.claim(longTimeout));
-    Completion completion = shutdown(pool);
-    return completion;
+    return shutdown(pool);
   }
   
   /**
@@ -1030,8 +1037,7 @@ public class PoolTest {
     Pool<GenericPoolable> pool = fixture.initPool(config);
     shutdown(pool).await(longTimeout);
     Thread.currentThread().interrupt();
-    Completion completion = shutdown(pool);
-    return completion;
+    return shutdown(pool);
   }
   
   /**
@@ -1446,6 +1452,141 @@ public class PoolTest {
     Pool<GenericPoolable> pool = fixture.initPool(config);
     GenericPoolable obj = forkFuture($claim(pool, longTimeout)).get();
     obj.release();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  @Theory public void
+  targetSizeMustBeGreaterThanZero(PoolFixture fixture) {
+    ResizablePool<GenericPoolable> pool = resizable(fixture);
+    pool.setTargetSize(0);
+  }
+
+  @Theory public void
+  targetSizeMustBeConfiguredSizeByDefault(PoolFixture fixture) {
+    config.setSize(23);
+    ResizablePool<GenericPoolable> pool = resizable(fixture);
+    assertThat(pool.getTargetSize(), is(23));
+  }
+
+  @Theory public void
+  getTargetSizeMustReturnLastSetTargetSize(PoolFixture fixture) {
+    ResizablePool<GenericPoolable> pool = resizable(fixture);
+    pool.setTargetSize(3);
+    assertThat(pool.getTargetSize(), is(3));
+  }
+
+  /**
+   * When we increase the size of a depleted pool, it should be possible to
+   * make claim again and get out newly allocated objects.
+   *
+   * We test for this by depleting a pool, upping the size and then claiming
+   * again with a timeout that is longer than the timeout of the test. The test
+   * pass if it does not timeout.
+   */
+  @Test(timeout = 300)
+  @Theory public void
+  increasingSizeMustAllowMoreAllocations(PoolFixture fixture) throws Exception {
+    ResizablePool<GenericPoolable> pool = resizable(fixture);
+    pool.claim(longTimeout); // depleted
+    pool.setTargetSize(2);
+    // now this mustn't block:
+    pool.claim(longTimeout);
+  }
+
+  /**
+   * We must somehow ensure that the pool starts deallocating more than it
+   * allocates, when the pool is shrunk. This is difficult because the pool
+   * cannot tell us when it reaches the target size, so we have to figure this
+   * out by using a special allocator.
+   *
+   * We test for this by configuring a CountingAllocator that also unparks a
+   * thread (namely ours, the main thread for the test) at every allocation
+   * and deallocation. We also configure the pool to have a somewhat large
+   * initial size, so we can shrink it later. Then we deplete the pool, and
+   * set a smaller target size. After setting the new target size, we release
+   * just enough objects for the pool to reach it, and then we wait for the
+   * allocator to register that same number of deallocations. This has to
+   * happen before the test times out. After that, we check that the difference
+   * between the allocations and the deallocations matches the new target size.
+   */
+  @Test(timeout = 300)
+  @Theory public void
+  decreasingSizeMustEventuallyDeallocateSurplusObjects(PoolFixture fixture)
+      throws Exception {
+    int startingSize = 5;
+    int newSize = 1;
+    final Thread main = Thread.currentThread();
+    CountingAllocator allocator = new UnparkingCountingAllocator(main);
+    config.setSize(startingSize);
+    config.setAllocator(allocator);
+    ResizablePool<GenericPoolable> pool = resizable(fixture);
+    List<GenericPoolable> objs = new ArrayList<GenericPoolable>();
+
+    while (allocator.allocations() != startingSize) {
+      objs.add(pool.claim(longTimeout)); // force the pool to do work
+    }
+    pool.setTargetSize(newSize);
+    while (allocator.deallocations() != startingSize - newSize) {
+      if (objs.size() > 0) {
+        objs.remove(0).release(); // give the pool objects to deallocate
+      } else {
+        pool.claim(longTimeout).release(); // prod it & poke it
+      }
+      LockSupport.parkNanos(10000000); // 10 millis
+    }
+    assertThat(
+        allocator.allocations() - allocator.deallocations(), is(newSize));
+  }
+
+  /**
+   * Similar to the decreasingSizeMustEventuallyDeallocateSurplusObjects test
+   * above, but this time the objects are all expired after the pool has been
+   * shrunk.
+   *
+   * Again, we deplete the pool. Once depleted, our expiration has been
+   * configured such, that all subsequent items one tries to claim, will be
+   * expired.
+   *
+   * Then we set the new lower target size, and release just enough for the
+   * pool to reach the new target.
+   *
+   * Then we try to claim an object from the pool with a very short timeout.
+   * This will return null because the pool is still depleted. We also check
+   * that the pool has not made any new allocations, even though we have been
+   * releasing objects. We don't check the deallocations because it's
+   * complicated and we did it in the
+   * decreasingSizeMustEventuallyDeallocateSurplusObjects test above.
+   */
+  @Test(timeout = 300)
+  @Theory public void
+  mustNotReallocateWhenReleasingExpiredObjectsIntoShrunkPool(PoolFixture fixture)
+      throws Exception {
+    int startingSize = 5;
+    int newSize = 1;
+    CountingAllocator allocator = new CountingAllocator();
+    Expiration<Poolable> expiration = new CountingExpiration(
+        // our 5 items are not expired when we deplete the pool
+        false, false, false, false, false,
+        // but all items we try to claim after that *are* expired.
+        true
+    );
+    config.setExpiration(expiration).setAllocator(allocator);
+    config.setSize(startingSize);
+    ResizablePool<GenericPoolable> pool = resizable(fixture);
+    List<GenericPoolable> objs = new ArrayList<GenericPoolable>();
+    for (int i = 0; i < startingSize; i++) {
+      objs.add(pool.claim(longTimeout));
+    }
+    assertThat(objs.size(), is(startingSize));
+    pool.setTargetSize(newSize);
+    for (int i = 0; i < startingSize - newSize; i++) {
+      // release the surplus expired objects back into the pool
+      objs.remove(0).release();
+    }
+    // now the released objects should not cause reallocations, so claim
+    // returns null (it's still depleted) and allocation count stays put
+    assertThat(pool.claim(shortTimeout), nullValue());
+    assertThat(allocator.allocations(), is(startingSize));
   }
   
   // NOTE: When adding, removing or modifying tests, also remember to update
