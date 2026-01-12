0b8ec1b452cc2bce064481e591c352603e46ded0
Chris Vest <mr.chrisvest@gmail.com>
2026-01-11 20:23:37 -0800

    Disable a test that can't be fixed reliably

--- src/test/java/stormpot/tests/blackbox/ThreadBasedWithConcurrentAllocationPoolTest.java
+++ src/test/java/stormpot/tests/blackbox/ThreadBasedWithConcurrentAllocationPoolTest.java
@@ -1,5 +1,6 @@
 package stormpot.tests.blackbox;
 
+import org.junit.jupiter.api.Disabled;
 import stormpot.Allocator;
 import stormpot.Pool;
 import stormpot.PoolBuilder;
@@ -9,5 +10,11 @@ public class ThreadBasedWithConcurrentAllocationPoolTest extends AllocatorBasedP
   @Override
   protected <T extends Poolable> PoolBuilder<T> createInitialPoolBuilder(Allocator<T> allocator) {
     return Pool.fromThreaded(allocator).setMaxConcurrentAllocations(2);
+  }
+
+  @Disabled("This causes issues with leak detection where reference processing appear to be indefinitely delayed " +
+          "for the leaked object.")
+  @Override
+  void managedPoolMustCountLeakedObjects() {
   }
 }
